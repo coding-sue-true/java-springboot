485c0621b5ea847a1ff93b96c2de240cf13552bf
soraia.carmo <soraia.carmo@10xbanking.com>
2019-05-14 14:18:50 +0100

    initial commit - spring boot with GET, POST, PUT and Delete endpoints configured - Port 8081

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,43 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("java", "Spring Framework", "Spring Framework Description2"),
+            new Topic("app", "Spring Framework", "Spring Framework Description3")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopics(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+}
